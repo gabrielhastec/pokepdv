2f774724320de3ac80839d3155013d306ee8d59a
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-04-30 15:04:30 -0300

    setup(api): init Spring Boot project with Clean Architecture.

---
+++ pokepdv-api/src/main/java/dev/pokepay/api/PokepdvApiApplication.java
@@ -0,0 +1,13 @@
+package dev.pokepay.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokepdvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokepdvApiApplication.class, args);
+	}
+
+}
