2b15235d56e4fbf6dfbe94b94b826d2f3d771ecf
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-05-03 19:26:10 -0300

    feat(admin): Adiciona Toaster global no App.

--- pokepdv-admin/src/App.tsx
+++ pokepdv-admin/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './features/auth/AuthContext';
 import AppRouter from './router';
+import { Toaster } from 'react-hot-toast';
 
 export default function App() {
 
   return (
       <BrowserRouter>
-        <AuthProvider>
-          <AppRouter />
-        </AuthProvider>
+          <AuthProvider>
+              <AppRouter />
+              <Toaster position="top-right" />
+          </AuthProvider>
       </BrowserRouter>
   );
 
